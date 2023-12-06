55fc5bb19e9c296c2491a0b0503e66d2dc3a0c01
jerocen <jeronimo17917@gmail.com>
2023-12-06 10:23:01 -0600

    Vista de la información de la planta, además del diseño.

--- app/src/main/java/com/example/interfaces_integradora/DetallePlanta.java
+++ app/src/main/java/com/example/interfaces_integradora/DetallePlanta.java
@@ -19,13 +19,12 @@ public class DetallePlanta extends AppCompatActivity {
         if (intent != null) {
             ItemPlant selectedPlant = (ItemPlant) intent.getSerializableExtra("PLANTA_SELECCIONADA");
 
-            // Aquí puedes usar los datos de la planta seleccionada para mostrar detalles
+
             if (selectedPlant != null) {
-                // Por ejemplo, puedes mostrar el nombre de la planta en un TextView
+
                 TextView textViewNombre = findViewById(R.id.textview_nombre_planta);
                 textViewNombre.setText(selectedPlant.getNombre());
 
-                // También puedes mostrar la imagen de la planta en un ImageView
                 ImageView imageViewPlanta = findViewById(R.id.imageview_planta);
                 imageViewPlanta.setImageResource(selectedPlant.getImage());
             }
