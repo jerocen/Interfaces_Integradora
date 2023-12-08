b16ce5bded25cded733148bca5e50cd501540c57
Arturo <hernandez020610@gmail.com>
2023-12-08 10:32:25 -0600

    cambio

--- app/src/main/java/com/example/interfaces_integradora/View/MyPlants.java
+++ app/src/main/java/com/example/interfaces_integradora/View/MyPlants.java
@@ -19,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -125,11 +126,11 @@ public class MyPlants extends AppCompatActivity implements NavigationView.OnNavi
         itemPlants.add(new ItemPlant("Narcisos", R.drawable.imagen_planta8));
         itemPlants.add(new ItemPlant("Dalias", R.drawable.imagen_planta9));
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         recyclerView.setAdapter(new PlantsAdaptador(getApplicationContext(),itemPlants));
 
         PlantsAdaptador adapter = new PlantsAdaptador(getApplicationContext(), itemPlants);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         recyclerView.setAdapter(adapter);
     }
 
