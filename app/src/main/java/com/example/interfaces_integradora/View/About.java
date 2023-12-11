db09131b3f121ada27e0de1eb67c816e8920e9bf
Arturo <hernandez020610@gmail.com>
2023-12-10 19:15:01 -0600

    about

--- app/src/main/java/com/example/interfaces_integradora/View/About.java
+++ app/src/main/java/com/example/interfaces_integradora/View/About.java
@@ -1,16 +1,83 @@
 package com.example.interfaces_integradora.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.interfaces_integradora.Models.Peticiones;
 import com.example.interfaces_integradora.R;
+import com.example.interfaces_integradora.Utility.NetworkChangeListener;
+import com.example.interfaces_integradora.ViewModel.ViewModelMyPlant;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
-public class About extends AppCompatActivity {
+public class About extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    String token;
+
+    com.example.interfaces_integradora.Models.Peticiones Peticiones = new Peticiones();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        drawerLayout = findViewById(R.id.drawer_layout2);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_about);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_my_plants) {
+            Intent intent = new Intent(this, MyPlants.class);
+            startActivity(intent);
+        } else if (itemId == R.id.nav_perfil) {
+            Intent intent = new Intent(this, Perfil.class);
+            startActivity(intent);
+        } else if (itemId == R.id.nav_contact) {
+            Intent intent = new Intent(this, Contact.class);
+            startActivity(intent);
+        } else if (itemId == R.id.nav_logout) {
+            Log.d("MyPlants", "Token: " + token);
+            Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
+            Peticiones.logoutUser(this, token, () -> {
+                Intent intent = new Intent(this, LogInView.class);
+                startActivity(intent);
+                finish();
+            });
+        } else if (itemId == R.id.nav_about) {
+            Intent intent = new Intent(this, About.class);
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
