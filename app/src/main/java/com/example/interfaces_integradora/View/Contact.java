c6d64304e61deafd4235ab7d895a06729517412c
Arturo <hernandez020610@gmail.com>
2023-12-10 19:45:10 -0600

    hola my father

--- app/src/main/java/com/example/interfaces_integradora/View/Contact.java
+++ app/src/main/java/com/example/interfaces_integradora/View/Contact.java
@@ -1,16 +1,126 @@
 package com.example.interfaces_integradora.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.interfaces_integradora.Models.Peticiones;
 import com.example.interfaces_integradora.R;
+import com.example.interfaces_integradora.ViewModel.ViewModelMyPlant;
+import com.google.android.material.navigation.NavigationView;
 
-public class Contact extends AppCompatActivity {
+public class Contact extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    SharedPreferences sharedPreferences;
+    String token;
+
+    ViewModelMyPlant viewModel;
+
+    com.example.interfaces_integradora.Models.Peticiones Peticiones = new Peticiones();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        token = sharedPreferences.getString("token", "");
+
+        drawerLayout = findViewById(R.id.drawer_layout4);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        View headerView = navigationView.getHeaderView(0);
+        TextView nombrePerfilTextView = headerView.findViewById(R.id.nombreperfil);
+        TextView correoTextView = headerView.findViewById(R.id.correo);
+
+        viewModel = new ViewModelProvider(this).get(ViewModelMyPlant.class);
+
+        viewModel.getNombrePerfil().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String nombrePerfil) {
+                nombrePerfilTextView.setText(nombrePerfil);
+            }
+        });
+
+        viewModel.getToastMessage().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String toastMessage) {
+                Toast.makeText(Contact.this, toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewModel.getCorreo().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String correo) {
+                correoTextView.setText(correo);
+            }
+        });
+        viewModel.obtenerDatosUser(token);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_contact);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer((GravityCompat.START));
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_my_plants) {
+            Intent intent = new Intent(this, MyPlants.class);
+            startActivity(intent);
+        } else if (itemId == R.id.nav_perfil) {
+            Intent intent = new Intent(this, Perfil.class);
+            startActivity(intent);
+        } else if (itemId == R.id.nav_contact) {
+            Intent intent = new Intent(this, Contact.class);
+            startActivity(intent);
+        } else if (itemId == R.id.nav_logout) {
+            Log.d("MyPlants", "Token: " + token);
+            Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
+            Peticiones.logoutUser(this, token, () -> {
+                Intent intent = new Intent(this, LogInView.class);
+                startActivity(intent);
+                finish();
+            });
+        } else if (itemId == R.id.nav_about) {
+            Intent intent = new Intent(this, About.class);
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
