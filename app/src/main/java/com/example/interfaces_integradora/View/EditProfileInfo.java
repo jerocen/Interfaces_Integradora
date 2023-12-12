47115dddf8a76ce91a8a8df0f4ebcbb2cf777e1e
Arturo <hernandez020610@gmail.com>
2023-12-12 16:03:53 -0600

    hola

--- app/src/main/java/com/example/interfaces_integradora/View/EditProfileInfo.java
+++ app/src/main/java/com/example/interfaces_integradora/View/EditProfileInfo.java
@@ -6,11 +6,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.interfaces_integradora.Models.Peticiones;
 import com.example.interfaces_integradora.R;
 import com.example.interfaces_integradora.Retrofit.ResponsePostUserChangePassword;
+
+import org.json.JSONObject;
 
 import retrofit2.Call;
 
@@ -23,6 +26,7 @@ public class EditProfileInfo extends AppCompatActivity {
     EditText newpasswordconfirm;
     Button btnEdit;
 
+    TextView tvErrorMessage2;
     Peticiones peticiones = new Peticiones();
 
     @Override
@@ -36,12 +40,13 @@ public class EditProfileInfo extends AppCompatActivity {
         newpassword = findViewById(R.id.newPassword);
         newpasswordconfirm = findViewById(R.id.Confirmpassword);
         btnEdit = findViewById(R.id.btnConfirmarCambio);
+        tvErrorMessage2 = findViewById(R.id.tvErrorMessage2);
 
         btnEdit.setOnClickListener(v -> {
             String password = newpassword.getText().toString();
             String passwordConfirm = newpasswordconfirm.getText().toString();
 
-            if (password.equals(passwordConfirm)) {
+            if (!password.isEmpty() && !passwordConfirm.isEmpty()) {
                 Call<ResponsePostUserChangePassword> call = peticiones.changePassword(token, password, passwordConfirm);
 
                 call.enqueue(new retrofit2.Callback<ResponsePostUserChangePassword>() {
@@ -49,16 +54,28 @@ public class EditProfileInfo extends AppCompatActivity {
                     public void onResponse(Call<ResponsePostUserChangePassword> call, retrofit2.Response<ResponsePostUserChangePassword> response) {
                         if (response.isSuccessful()) {
                             Toast.makeText(EditProfileInfo.this, response.body().getMsg(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            if (response.errorBody() != null) {
+                                try {
+                                    JSONObject jObjError = new JSONObject(response.errorBody().string());
+                                    String errorMessage = jObjError.getJSONObject("data").getJSONArray("password").getString(0);
+                                    tvErrorMessage2.setText(errorMessage);
+                                } catch (Exception e) {
+                                    tvErrorMessage2.setText("Error en la validación de datos");
+                                }
+                            } else {
+                                tvErrorMessage2.setText("La respuesta es nula");
+                            }
                         }
                     }
 
                     @Override
                     public void onFailure(Call<ResponsePostUserChangePassword> call, Throwable t) {
-                        Toast.makeText(EditProfileInfo.this, "Error al cambiar la contraseña", Toast.LENGTH_SHORT).show();
-
+                        tvErrorMessage2.setText("Error al cambiar la contraseña");
                     }
                 });
-
+            } else {
+                tvErrorMessage2.setText("La contraseña y la confirmación de la contraseña son obligatorias");
             }
         });
 
